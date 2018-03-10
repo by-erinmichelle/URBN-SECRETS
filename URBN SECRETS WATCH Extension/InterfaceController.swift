34980eafbf1c418e929d2827a2c4d05d545c92a9
Erin Michelle <emw337@drexel.edu>
2018-03-10 13:00:29 -0500

    Initial setup

---
+++ URBN SECRETS WATCH Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  URBN SECRETS WATCH Extension
+//
+//  Created by Erin Wiegman on 3/10/18.
+//  Copyright © 2018 Erin Wiegman. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
