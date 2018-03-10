e866ab608973469bad517d3d57824ca13f9df9cf
Erin Michelle <emw337@drexel.edu>
2018-03-10 14:13:15 -0500

    added proximity rule

--- URBN SECRETS/ViewController.swift
+++ URBN SECRETS/ViewController.swift
@@ -13,19 +13,22 @@ class ViewController: UIViewController, ESTTriggerManagerDelegate {
     // 2. Add the trigger manager
     let triggerManager = ESTTriggerManager()
 
-    let colors = [UIColor(red: 84/255, green: 77/255, blue: 160/255, alpha: 1), UIColor(red: 84/255, green: 77/255, blue: 160/255, alpha: 1)]
+    let colors = [UIColor(red: 84/255, green: 77/255, blue: 160/255, alpha: 1), UIColor(red: 255/255, green: 0/255, blue: 0/255, alpha: 1)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // 3. Set the trigger manager's delegate
         self.triggerManager.delegate = self
-        let rule1 = ESTOrientationRule.orientationEquals(
+        let rule2 = ESTOrientationRule.orientationEquals(
             .horizontalUpsideDown, for: .bag)
-        let rule2 = ESTMotionRule.motionStateEquals(
-            true, forNearableIdentifier: "ee84b8102024b5b3")
+//        changed 20 ft away
+        let rule1 = ESTProximityRule.inRangeOf(.car)
+//        let rule2 = ESTMotionRule.motionStateEquals(
+//            true, forNearableIdentifier: "ee84b8102024b5b3")
+
         
-        let trigger = ESTTrigger(rules: [rule1, rule2], identifier: "tom the trigger")
+        let trigger = ESTTrigger(rules: [rule1, rule2], identifier: "trigger")
         
         self.triggerManager.startMonitoring(for: trigger)
     }
@@ -37,12 +40,11 @@ class ViewController: UIViewController, ESTTriggerManagerDelegate {
 
     func triggerManager(_ manager: ESTTriggerManager,
                         triggerChangedState trigger: ESTTrigger) {
-        if (trigger.identifier == "tom the trigger" && trigger.state == true) {
-            //print("Hello, digital world! The physical world has spoken.")
-            self.view.backgroundColor = self.colors[0]
+        if (trigger.identifier == "trigger" && trigger.state == true) {
+            self.view.backgroundColor = self.colors[1]
             
         } else {
-            print("Goodnight")
+//            print("Goodnight")
         }
     }
 }
